f85f3d4628e5f772b6a4aabd2173b7abd27ed7c0
himazin331 <sion20022013@gmail.com>
2023-06-01 06:58:20 +0900

    目次が正しく表示されない問題の解消とロジック改善

--- src/components/parts/article_outline.tsx
+++ src/components/parts/article_outline.tsx
@@ -1,5 +1,6 @@
 /* 記事目次 */
 
+import { useRouter } from "next/router";
 import React, {useEffect, useState} from "react";
 import style from "@/styles/blog.module.css";
 
@@ -10,32 +11,42 @@ interface headingData {
 }
 
 const ArticleOutline: React.FC = (): React.ReactElement => {
+  const asPath: string = useRouter().asPath;
   const [headings, setHeadings] = useState<headingData[]>([]);
 
   useEffect(() => {
     const articleBody: HTMLElement  = document.getElementById("article_body_field") || document.body;
     const headingTags: HTMLHeadingElement[] = Array.from(articleBody.querySelectorAll("h2, h3, h4, h5")) as HTMLHeadingElement[];
-    const headingData: headingData[] = headingTags.map((htag: HTMLHeadingElement) => {
-      return {
-        id: htag.id,
-        tag: htag.tagName.toLowerCase(),
-        text: htag.innerText
-      };
-    });
-
-    setHeadings(headingData);
-  }, []);
+    if (headingTags.length > 0)
+    {
+      const headingData: headingData[] = headingTags.map((htag: HTMLHeadingElement) => {
+        return {
+          id: htag.id,
+          tag: htag.tagName.toLowerCase(),
+          text: htag.innerText
+        };
+      });
+      setHeadings(headingData);
+    } else {
+      setHeadings([]);
+    }
+  }, [asPath]);
 
   return (
-    <div className={style.article_outline}>
-      <ul>
-        {headings.map((heading: headingData, idx: number) => (
-          <li key={idx}>
-            <a href={`#${heading.id}`}>{heading.text}</a>
-          </li>
-        ))}
-      </ul>
-    </div>
+    headings.length > 0? 
+    <>
+      <div className={style.article_outline}>
+        <ul>
+          {headings.map((heading: headingData, idx: number) => (
+            <li key={idx}>
+              <a href={`#${heading.id}`}>{heading.text}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+    :
+    <></>
   );
 };
 export default ArticleOutline;
