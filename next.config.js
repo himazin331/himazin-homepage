2ec8a2ae51f632ebd61cd23c822e4fc172f4b9f0
himazin331 <sion20022013@gmail.com>
2023-06-06 22:03:28 +0900

    Security Headersの設定

--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
 /** @type {import('next').NextConfig} */
+const headers = require('./headers');
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['github-readme-stats.vercel.app', 'images.microcms-assets.io'],
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers,
+      },
+    ];
   }
 };
 
